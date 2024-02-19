957acb1700fb6590fe8ffc6c07fb99e4dd4206dc
maideergul <maideergul@yahoo.com>
2024-02-19 15:42:49 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/Work.java
@@ -0,0 +1,60 @@
+package com.example.demo;
+
+public class Work {
+
+    int num50=0;
+    int num25=0;
+    int num10=0;
+    int num5=0;
+    int num1=0;
+    public void countCoins(int value) {
+
+        if(value >= 0)
+        {
+            //Math.
+            num50= (value/50);
+            value = (value%50);
+
+        }
+        if(value >= 25)
+        {
+             num25 = (value/25);
+             value = (value%25);
+        }
+        if(value >= 10)
+        {
+             num10 = (value/10);
+             value = (value%10);
+        }
+        if(value >= 5)
+        {
+             num5 = (value/5);
+            value = (value%5);
+        }
+        if(value >= 1)
+        {
+             num1 = value/(1);
+             value = (value%1);
+        }
+
+        System.out.println("50 : " + num50 + " 25 : " + num25 + " 10 : " + num10 + " 5 : " + num5 + " 1 : " + num1);
+
+
+    }
+    public static void main(String[] args) {
+
+        Work work = new Work();
+
+        work.countCoins(381);
+
+    }
+
+}
+// 50, 25, 10, 5, 1
+
+
+
+
+
+
+
